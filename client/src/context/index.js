08d75f96c8f4bf68c6bfc5806da11551ac7e1fe7
Zach Seaman <znseaman@gmail.com>
2019-11-06 10:22:15 -0800

    improv(dx): remove unnecessary Context methods

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -2,10 +2,7 @@ import { createContext } from "react";
 
 const ItemsContext = createContext({
   items: [],
-  setItems: f => f,
-  onCheck: f => f,
-  onRemove: f => f,
-  addNewItem: f => f
+  setItems: f => f
 });
 
 export default ItemsContext;
